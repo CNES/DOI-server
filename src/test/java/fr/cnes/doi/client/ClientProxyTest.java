28d3cb2df85e69009512825de92fd7438a4a75c1
Jean-Christophe Malapert <jean-christophe.malapert@cnes.fr>
2017-07-07 14:15:21 +0200

    Test again

--- src/test/java/fr/cnes/doi/client/ClientProxyTest.java
+++ src/test/java/fr/cnes/doi/client/ClientProxyTest.java
@@ -6,6 +6,7 @@
 package fr.cnes.doi.client;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -18,9 +19,14 @@ import org.apache.http.HttpHost;
 import org.apache.http.HttpResponse;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.config.AuthSchemes;
+import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.conn.params.ConnRoutePNames;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.protocol.BasicHttpContext;
+import org.apache.http.protocol.HttpContext;
 
 /**
  *
@@ -58,39 +64,43 @@ public class ClientProxyTest {
 //        HttpClient client = builder.build();
 //        HttpUriRequest httpRequest = new HttpGet("http://www.google.com");
 //        HttpResponse response = client.execute(httpRequest);        
-       DefaultHttpClient httpclient = new DefaultHttpClient();
 
+DefaultHttpClient httpclient = new DefaultHttpClient();
+        HttpContext localContext = new BasicHttpContext();
+ 
         httpclient.getCredentialsProvider().setCredentials(
-                new AuthScope("proxy-HTTP2.cnes.fr", 8050), 
-                new UsernamePasswordCredentials("username", "password"));
-
-        HttpHost targetHost = new HttpHost("www.google.fr", 80, "http"); 
-        HttpHost proxy = new HttpHost("proxy-HTTP2.cnes.fr", 8050); 
-
+                new AuthScope("proxy-HTTP2.cnes.fr", 8050),
+                new UsernamePasswordCredentials("", ""));
+ 
+        HttpHost targetHost = new HttpHost("www.google.fr", 80, "http");
+        HttpHost proxy = new HttpHost("proxy-HTTP2.cnes.fr", 8050);
+ 
         httpclient.getParams().setParameter(ConnRoutePNames.DEFAULT_PROXY, proxy);
-
-        HttpGet httpget = new HttpGet("/");
-        
-        System.out.println("executing request: " + httpget.getRequestLine());
+  
         System.out.println("via proxy: " + proxy);
         System.out.println("to target: " + targetHost);
-        
-        HttpResponse response = httpclient.execute(targetHost, httpget);
+ 
+        RequestConfig config = RequestConfig.custom()
+        .setTargetPreferredAuthSchemes(Arrays.asList(AuthSchemes.BASIC, AuthSchemes.NTLM))
+        .build();
+        HttpGet target = new HttpGet("http://www.google.fr");
+        target.setConfig(config);
+        HttpResponse response = httpclient.execute(target);
         HttpEntity entity = response.getEntity();
-
+ 
         System.out.println("----------------------------------------");
         System.out.println(response.getStatusLine());
         if (entity != null) {
             System.out.println("Response content length: " + entity.getContentLength());
         }
-        if (entity != null) {
+       if (entity != null) {
             entity.consumeContent();
         }
-        
-        // When HttpClient instance is no longer needed, 
+ 
+        // When HttpClient instance is no longer needed,
         // shut down the connection manager to ensure
         // immediate deallocation of all system resources
-        httpclient.getConnectionManager().shutdown();      
+        httpclient.getConnectionManager().shutdown();   
 
     }
 
