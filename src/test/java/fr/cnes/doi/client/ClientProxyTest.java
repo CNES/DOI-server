dfb9f0fd38a72b687c0bdd19bdca7aeb87f42739
Jean-Christophe Malapert <jean-christophe.malapert@cnes.fr>
2017-08-25 18:03:57 +0200

    Add a test to check https request through http proxy

--- src/test/java/fr/cnes/doi/client/ClientProxyTest.java
+++ src/test/java/fr/cnes/doi/client/ClientProxyTest.java
@@ -24,6 +24,11 @@ import org.restlet.resource.ClientResource;
 import fr.cnes.doi.InitSettingsForTest;
 import fr.cnes.doi.settings.Consts;
 import fr.cnes.doi.settings.DoiSettings;
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.UsernamePasswordCredentials;
+import org.apache.commons.httpclient.auth.AuthScope;
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.junit.Ignore;
 
 /**
  *
@@ -31,117 +36,134 @@ import fr.cnes.doi.settings.DoiSettings;
  */
 public class ClientProxyTest {
 
-	/**
-	 * Init the settings
-	 */
-	@BeforeClass
-	public static void setUpClass() {
-		InitSettingsForTest.init();
-	}
+    /**
+     * Init the settings
+     */
+    @BeforeClass
+    public static void setUpClass() {
+        InitSettingsForTest.init();
+    }
 
-	/**
-	 * Executed after the test class
-	 */
-	@AfterClass
-	public static void tearDownClass() {
-	}
+    /**
+     * Executed after the test class
+     */
+    @AfterClass
+    public static void tearDownClass() {
+    }
 
-	/**
-	 * Executed before each test
-	 */
-	@Before
-	public void setUp() {
-	}
+    /**
+     * Executed before each test
+     */
+    @Before
+    public void setUp() {
+    }
 
-	/**
-	 * Executed after each test
-	 */
-	@After
-	public void tearDown() {
-	}
+    /**
+     * Executed after each test
+     */
+    @After
+    public void tearDown() {
+    }
 
-	/**
-	 * Test the connection through the proxy. Works only if the test is executed
-	 * behind a proxy
-	 * 
-	 * @throws Exception
-	 */
-	@Test
-	public void testProxy() throws Exception {
+    /**
+     * Test the connection through the proxy. Works only if the test is executed
+     * behind a proxy
+     *
+     * @throws Exception
+     */
+    @Test
+    @Ignore
+    public void testProxy() throws Exception {
 
-		// Execute only if proxy is enabled
-		if (DoiSettings.getInstance().getBoolean(Consts.SERVER_PROXY_USED)) {
-			Engine.getInstance().getRegisteredClients().clear();
-			Engine.getInstance().getRegisteredClients().add(new HttpClientHelper(null));
+        // Execute only if proxy is enabled
+        if (DoiSettings.getInstance().getBoolean(Consts.SERVER_PROXY_USED)) {
+            Engine.getInstance().getRegisteredClients().clear();
+            Engine.getInstance().getRegisteredClients().add(new HttpClientHelper(null));
 
-			Client proxy = new Client(new Context(), Protocol.HTTP);
-			proxy.getContext().getParameters().add("proxyHost",
-					DoiSettings.getInstance().getString(Consts.SERVER_PROXY_HOST));
-			proxy.getContext().getParameters().add("proxyPort",
-					DoiSettings.getInstance().getString(Consts.SERVER_PROXY_PORT));
+            Client proxy = new Client(new Context(), Protocol.HTTP);
+            proxy.getContext().getParameters().add("proxyHost",
+                    DoiSettings.getInstance().getString(Consts.SERVER_PROXY_HOST));
+            proxy.getContext().getParameters().add("proxyPort",
+                    DoiSettings.getInstance().getString(Consts.SERVER_PROXY_PORT));
 
-			ClientResource client = new ClientResource("http://www.google.fr");
-			client.setProxyChallengeResponse(ChallengeScheme.HTTP_BASIC,
-					DoiSettings.getInstance().getSecret(Consts.SERVER_PROXY_LOGIN),
-					DoiSettings.getInstance().getSecret(Consts.SERVER_PROXY_PWD));
-			client.setNext(proxy);
+            ClientResource client = new ClientResource("http://www.google.fr");
+            client.setProxyChallengeResponse(ChallengeScheme.HTTP_BASIC,
+                    DoiSettings.getInstance().getSecret(Consts.SERVER_PROXY_LOGIN),
+                    DoiSettings.getInstance().getSecret(Consts.SERVER_PROXY_PWD));
+            client.setNext(proxy);
 
-			Representation rep = client.get();
-			Status status = client.getStatus();
-			Assert.assertTrue("Test si la requete est OK", status.isSuccess());
+            Representation rep = client.get();
+            Status status = client.getStatus();
+            Assert.assertTrue("Test si la requete est OK", status.isSuccess());
 
-			System.out.println(rep.getText());
-		} else {
-			System.out.println("Proxy not enabled, no test");
-			Assert.assertTrue("No test executed", true);
-		}
+            System.out.println(rep.getText());
+        } else {
+            System.out.println("Proxy not enabled, no test");
+            Assert.assertTrue("No test executed", true);
+        }
 
-	}
+    }
 
-	/**
-	 * Test the connection through the proxy with BaseClient. Works only if the
-	 * test is executed behind a proxy
-	 * 
-	 * @throws Exception
-	 */
-	@Test
-	public void testBaseClient() throws Exception {
-		BaseClient baseClient = new BaseClient("http://www.google.fr");
-		if (DoiSettings.getInstance().getBoolean(Consts.SERVER_PROXY_USED)) {
-			baseClient.setProxyAuthentication(DoiSettings.getInstance().getString(Consts.SERVER_PROXY_HOST),
-					DoiSettings.getInstance().getString(Consts.SERVER_PROXY_PORT),
-					DoiSettings.getInstance().getSecret(Consts.SERVER_PROXY_LOGIN),
-					DoiSettings.getInstance().getSecret(Consts.SERVER_PROXY_PWD));
-		}
+    /**
+     * Test the connection through the proxy with BaseClient. Works only if the
+     * test is executed behind a proxy
+     *
+     * @throws Exception
+     */
+    @Test
+    @Ignore
+    public void testBaseClient() throws Exception {
+        BaseClient baseClient = new BaseClient("http://www.google.fr");
+        if (DoiSettings.getInstance().getBoolean(Consts.SERVER_PROXY_USED)) {
+            baseClient.setProxyAuthentication(DoiSettings.getInstance().getString(Consts.SERVER_PROXY_HOST),
+                    DoiSettings.getInstance().getString(Consts.SERVER_PROXY_PORT),
+                    DoiSettings.getInstance().getSecret(Consts.SERVER_PROXY_LOGIN),
+                    DoiSettings.getInstance().getSecret(Consts.SERVER_PROXY_PWD));
+        }
 
-		Representation rep = baseClient.client.get();
-		Status status = baseClient.client.getStatus();
-		Assert.assertTrue("Test si la requete est OK", status.isSuccess());
+        Representation rep = baseClient.client.get();
+        Status status = baseClient.client.getStatus();
+        Assert.assertTrue("Test si la requete est OK", status.isSuccess());
 
-		System.out.println(rep.getText());
+        System.out.println(rep.getText());
 
-	}
+    }
 
-	/**
-	 * Test the connection through the proxy with BaseClient and crossCite URL.
-	 * Works only if the test is executed behind a proxy
-	 * 
-	 * @throws Exception
-	 */
-	@Test
-	public void testBaseClientCrossCite() throws Exception {
-		BaseClient baseClient = new BaseClient("http://citation.crosscite.org/styles");
-		if (DoiSettings.getInstance().getBoolean(Consts.SERVER_PROXY_USED)) {
-			baseClient.setProxyAuthentication(DoiSettings.getInstance().getString(Consts.SERVER_PROXY_HOST),
-					DoiSettings.getInstance().getString(Consts.SERVER_PROXY_PORT),
-					DoiSettings.getInstance().getSecret(Consts.SERVER_PROXY_LOGIN),
-					DoiSettings.getInstance().getSecret(Consts.SERVER_PROXY_PWD));
-		}
+    /**
+     * Test the connection through the proxy with BaseClient and crossCite URL.
+     * Works only if the test is executed behind a proxy
+     *
+     * @throws Exception
+     */
+    @Test
+    @Ignore
+    public void testBaseClientCrossCite() throws Exception {
+        BaseClient baseClient = new BaseClient("http://citation.crosscite.org/styles");
+        if (DoiSettings.getInstance().getBoolean(Consts.SERVER_PROXY_USED)) {
+            baseClient.setProxyAuthentication(DoiSettings.getInstance().getString(Consts.SERVER_PROXY_HOST),
+                    DoiSettings.getInstance().getString(Consts.SERVER_PROXY_PORT),
+                    DoiSettings.getInstance().getSecret(Consts.SERVER_PROXY_LOGIN),
+                    DoiSettings.getInstance().getSecret(Consts.SERVER_PROXY_PWD));
+        }
 
-		Representation rep = baseClient.client.get();
-		Status status = baseClient.client.getStatus();
-		Assert.assertTrue("Test si la requete est OK", status.isSuccess());
+        Representation rep = baseClient.client.get();
+        Status status = baseClient.client.getStatus();
+        Assert.assertTrue("Test si la requete est OK", status.isSuccess());
 
-	}
+    }
+
+    @Test
+    public void testHttpClient() throws Exception {
+        HttpClient httpclient = new HttpClient();
+        if (DoiSettings.getInstance().getBoolean(Consts.SERVER_PROXY_USED)) {
+            httpclient.getHostConfiguration().setProxy(DoiSettings.getInstance().getString(Consts.SERVER_PROXY_HOST), Integer.valueOf(DoiSettings.getInstance().getString(Consts.SERVER_PROXY_PORT)));
+            httpclient.getState().setProxyCredentials("my-proxy-realm", DoiSettings.getInstance().getString(Consts.SERVER_PROXY_HOST),
+                    new UsernamePasswordCredentials(DoiSettings.getInstance().getSecret(Consts.SERVER_PROXY_LOGIN), DoiSettings.getInstance().getSecret(Consts.SERVER_PROXY_PWD)));
+        }
+        
+        GetMethod request = new GetMethod("https://www.google.com");
+        Assert.assertEquals("Test si la requete est OK", 200, request.getStatusCode());
+
+    }
 
 }
