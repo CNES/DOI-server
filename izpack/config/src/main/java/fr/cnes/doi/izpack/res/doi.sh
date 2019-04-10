79aba152fe0a8ad66b2878cdf2393016b553900b
Jean-Christophe Malapert <jean-christophe.malapert@cnes.fr>
2019-04-10 18:36:43 +0200

    Add a configuration file for systemd - Closes: #25

--- izpack/config/src/main/java/fr/cnes/doi/izpack/res/doi.sh
+++ izpack/config/src/main/java/fr/cnes/doi/izpack/res/doi.sh
@@ -2,20 +2,25 @@
 case "$1" in
     start)
             echo -n "Starting DOI-server"
-            java -Dlog4j.configurationFile=./log4j2.xml -jar DOI.jar -f config.properties --start
+            cd ${INSTALL_PATH}
+            java -Dlog4j.configurationFile=./log4j2.xml -jar DOI-server.jar -f config.properties --start
             ;;
    stop) 
           echo -n "Stopping DOI-server"
+          cd ${INSTALL_PATH}
           java -jar DOI.jar -f config.properties --stop
            ;;
    restart)
           echo -n "Stopping DOI-server"
+          cd ${INSTALL_PATH}
           java -jar DOI.jar -f config.properties --stop
           echo -n "Starting SOI-server"
+          cd ${INSTALL_PATH}
           java -Dlog4j.configurationFile=./log4j2.xml -jar DOI-server.jar -f config.properties --start
           ;;
    version)
           echo -n "DOI-server version"
+          cd ${INSTALL_PATH}
           java -Dlog4j.configurationFile=./log4j2.xml -jar DOI-server.jar -f config.properties --version
           ;;
         *)
